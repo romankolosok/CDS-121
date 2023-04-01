9d454030cd1c5f1678c282128dbdc3c867c54f85
romankolosok <romakolosok14@gmail.com>
2023-04-01 11:59:33 -0400

    assignment #14 finished

--- Assignment #14/assignment.py
+++ Assignment #14/assignment.py
@@ -1,10 +1,19 @@
 import re
 
-ip_expression = '(?<=[\D])((((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.)){3})((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])))(?=\D)'
+ip_expression = '(?<=\D)((((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.)){3})((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])))(?=\D)'
 with open("IP_search.txt") as file:
     ip_list = []
     for line in file.readlines():
-        # ip_list.extend([element[0] for element in ])
-        print(re.findall(ip_expression, line))
+        ip_list += [element[0] for element in re.findall(ip_expression, line)]
+
+    print(ip_list)
 
 #https://regex101.com/r/ttv7s2/1
+
+
+with open('pass_in.txt') as input_f:
+    with open('pass_out.txt', 'w') as output_f:
+        for line in input_f.readlines():
+            for match in re.findall(r'((?<=Password: )\w+)', line):
+                line = re.sub(match, "*" * len(match), line)
+            output_f.write(line)
